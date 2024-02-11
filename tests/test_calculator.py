dfe7c1efbd124e02439d902f4f6a720650a59a8c
ck378 <ck378@njit.edu>
2024-02-11 08:42:51 -0500

    added tests folder, with init py and testcalcpy.

---
+++ tests/test_calculator.py
@@ -0,0 +1,16 @@
+from calculator import add, subtract, multiply, divide, exponential
+
+def test_addition(): # Testing addition
+    assert add(4,4) == 8
+
+def test_subtraction(): # Test subtraction
+    assert subtract(7,1) == 6
+
+def test_multiplication(): # test multiplication
+    assert multiply(1,2) == 2
+
+def test_division(): # test division
+    assert divide(4,2) == 2
+
+def test_exponents(): ## test exponent
+    assert exponential(2,2) == 4
