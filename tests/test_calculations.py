85cb34936231eff56bfb8e5a173a82cdc59f6061
chris-k-njit <ck378@njit.edu>
2024-02-17 23:05:29 -0500

    test calculations file for part 2 branch updated, added more files.

---
+++ tests/test_calculations.py
@@ -0,0 +1,23 @@
+from calculator.operations import add, subtract, multiply, divide, exponential
+# Keeping tests similar to main branch from previous homework.
+
+def test_addition(): # Testing addition
+    '''Testing addition function'''
+    assert add(4,4) == 8
+
+def test_subtraction(): # Test subtraction
+    '''Testing subtraction function'''
+    assert subtract(7,1) == 6
+
+def test_multiplication(): # test multiplication
+    '''Testing multiplication function'''
+    assert multiply(1,2) == 2
+
+def test_division(): # test division
+    '''Testing division function'''
+    assert divide(4,2) == 2
+
+def test_exponents(): ## test exponent
+    '''Testing exponential function'''
+    assert exponential(2,2) == 4
+    
