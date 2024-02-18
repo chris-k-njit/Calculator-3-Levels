d88a8cb814e17422f3c40db9067d744738ba96f5
chris-k-njit <ck378@njit.edu>
2024-02-17 23:28:52 -0500

    cleaning up code, moving calculation class to another file as seen before.

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -12,19 +12,8 @@
 
 # def exponential(x,y):
 #     return x**y
-#from calculator.calculation import Calculation
+from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
-
-class Calculation:
-    def __init__(self, x, y, operation):
-        self.x = x
-        self.y = y
-        self.operation = operation  # Store the operation function
-
-    def get_result(self):
-        # Call the stored operation with x and y
-        # Testing Professors code for part 2, making my own for part 3
-        return self.operation(self.x, self.y)
 
 class Calculator:
     @staticmethod
