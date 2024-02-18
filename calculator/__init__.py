319756a39b4e1304ab455d66c0f50049ff06076c
chris-k-njit <ck378@njit.edu>
2024-02-17 23:13:59 -0500

    almost done reviewing part2.

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,14 +1,34 @@
-def add(x,y):
-  return x + y
+# def add(x,y):
+#   return x + y
 
-def subtract(x,y):
-  return x - y
+# def subtract(x,y):
+#   return x - y
 
-def multiply(x,y): 
-  return x * y
+# def multiply(x,y): 
+#   return x * y
 
-def divide(x,y):   
-  return x / y
+# def divide(x,y):   
+#   return x / y
 
-def exponential(x,y):
-    return x**y
+# def exponential(x,y):
+#     return x**y
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+    @staticmethod
+    def add(x,y):
+        calculation = Calculation(x, y, add)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract(x,y):
+        calculation = Calculation(x, y, subtract)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply (x,y):
+        calculation = Calculation(x, y, multiply)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(x,y):
+        calculation = Calculation(x, y, divide)  # Pass the add function from calculator.operations
+        return calculation.get_result()
