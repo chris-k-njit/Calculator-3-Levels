a493d875cb1551948b6d3d3ad4a8da7ff9e5df81
chris-k-njit <ck378@njit.edu>
2024-02-17 23:17:27 -0500

    reference Caclulator folder for operations.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,18 @@
 '''Calculator Test'''
-from calculator import add, subtract, multiply, divide
+from calculator import Calculator # add, subtract, multiply, divide
 
 def test_addition(): # Testing addition
     '''Testing addition function'''
-    assert add(4,4) == 8
+    assert Calculator.add(4,4) == 8
 
 def test_subtraction(): # Test subtraction
     '''Testing subtraction function'''
-    assert subtract(7,1) == 6
+    assert Calculator subtract(7,1) == 6
 
 def test_multiplication(): # test multiplication
     '''Testing multiplication function'''
-    assert multiply(1,2) == 2
+    assert Calculator.multiply(1,2) == 2
 
 def test_division(): # test division
     '''Testing division function'''
-    assert divide(4,2) == 2
+    assert Calculator.divide(4,2) == 2
